03d9835667c925a4ff7a3a4fba1cbc1e0afb312e
chenzhizsqq <chenzhi@qq.com>
2023-03-20 09:57:22 +0900

    添加 NuGet 包 模型和数据库上下文类

---
+++ webApi/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace webApi
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
